d6f5b2a5dd42e14c9cc63a696eefef158e581fe1
Colin Bate <colin@colinbate.com>
2024-03-13 00:25:51 -0300

    Scaffolding for the project. SvelteKit.

---
+++ README.md
@@ -0,0 +1,10 @@
+# TTRPG.tools Accounts
+
+A simple app to provide a centralized account for all tools created by TTRPG.tools.
+
+Planned functionality
+
+- Login with Discord
+- Login with GitHub
+- SSO experience for all https://*.ttrpg.tools/ apps
+- Maybe providing a basic persistence layer for the apps to use
---
+++ src/routes/+page.svelte
@@ -0,0 +1,33 @@
+<div class="flex min-h-full flex-col justify-center py-12 sm:px-6 lg:px-8">
+  
+  <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-[480px]">
+    <div class="bg-white dark:bg-gray-950 px-6 py-12 shadow md:shadow-lg dark:shadow-purple-700/10 sm:rounded-lg sm:px-12">
+      
+      <div class="sm:mx-auto sm:w-full sm:max-w-md">
+        <svg xmlns="http://www.w3.org/2000/svg" class="mx-auto h-10 w-auto text-orange-500" fill="currentColor" viewBox="0 0 640 512"><path class="fa-secondary fill-purple-500" opacity=".4" d="M273.9 163.6c-30.4 9.4-53.2 36.5-56 69.8l-4.1 49.3c-2.5 29.8 15.7 56.1 42.2 65.6v73.8C218.6 458 167.9 480 112 480H96c-17.7 0-32-14.3-32-32c0-45.7 14.7-88 39.7-122.3C40.6 275.9 0 198.7 0 112V64C0 46.3 14.3 32 32 32c101.4 0 190.5 52.4 241.9 131.6zm92.3 0C417.5 84.4 506.6 32 608 32c17.7 0 32 14.3 32 32v48c0 86.7-40.6 163.9-103.7 213.7C561.3 360 576 402.3 576 448c0 17.7-14.3 32-32 32H528c-55.9 0-106.6-22-144-57.9V348.3c26.5-9.5 44.7-35.8 42.2-65.6l-4.1-49.3c-2.8-33.2-25.6-60.3-56-69.8z"/><path class="fa-primary" d="M256 64a64 64 0 1 1 128 0A64 64 0 1 1 256 64zM217.9 233.4c3.5-41.5 38.1-73.4 79.7-73.4h44.8c41.6 0 76.3 31.9 79.7 73.4l4.1 49.3c2.5 29.8-15.7 56.1-42.2 65.6V464c0 26.5-21.5 48-48 48H304c-26.5 0-48-21.5-48-48V348.3c-26.5-9.5-44.7-35.8-42.2-65.6l4.1-49.3z"/></svg>
+        <h2 class="mt-6 text-center text-2xl font-bold leading-9 tracking-tight">Sign in to your account</h2>
+      </div>
+
+      <div>
+
+        <div class="mt-12 grid grid-cols-2 gap-4">
+          <a href="#" class="flex w-full items-center justify-center gap-3 rounded-md bg-white dark:bg-gray-800 px-3 py-2 text-sm font-semibold shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 dark:hover:bg-gray-700 focus-visible:ring-transparent">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 512" class="size-5" fill="currentColor"><path d="M524.5 69.8a1.5 1.5 0 0 0 -.8-.7A485.1 485.1 0 0 0 404.1 32a1.8 1.8 0 0 0 -1.9 .9 337.5 337.5 0 0 0 -14.9 30.6 447.8 447.8 0 0 0 -134.4 0 309.5 309.5 0 0 0 -15.1-30.6 1.9 1.9 0 0 0 -1.9-.9A483.7 483.7 0 0 0 116.1 69.1a1.7 1.7 0 0 0 -.8 .7C39.1 183.7 18.2 294.7 28.4 404.4a2 2 0 0 0 .8 1.4A487.7 487.7 0 0 0 176 479.9a1.9 1.9 0 0 0 2.1-.7A348.2 348.2 0 0 0 208.1 430.4a1.9 1.9 0 0 0 -1-2.6 321.2 321.2 0 0 1 -45.9-21.9 1.9 1.9 0 0 1 -.2-3.1c3.1-2.3 6.2-4.7 9.1-7.1a1.8 1.8 0 0 1 1.9-.3c96.2 43.9 200.4 43.9 295.5 0a1.8 1.8 0 0 1 1.9 .2c2.9 2.4 6 4.9 9.1 7.2a1.9 1.9 0 0 1 -.2 3.1 301.4 301.4 0 0 1 -45.9 21.8 1.9 1.9 0 0 0 -1 2.6 391.1 391.1 0 0 0 30 48.8 1.9 1.9 0 0 0 2.1 .7A486 486 0 0 0 610.7 405.7a1.9 1.9 0 0 0 .8-1.4C623.7 277.6 590.9 167.5 524.5 69.8zM222.5 337.6c-29 0-52.8-26.6-52.8-59.2S193.1 219.1 222.5 219.1c29.7 0 53.3 26.8 52.8 59.2C275.3 311 251.9 337.6 222.5 337.6zm195.4 0c-29 0-52.8-26.6-52.8-59.2S388.4 219.1 417.9 219.1c29.7 0 53.3 26.8 52.8 59.2C470.7 311 447.5 337.6 417.9 337.6z"/></svg>
+            <span class="text-sm font-semibold leading-6">Discord</span>
+          </a>
+
+          <a href="#" class="flex w-full items-center justify-center gap-3 rounded-md bg-white dark:bg-gray-800 px-3 py-2 text-sm font-semibold shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 dark:hover:bg-gray-700 focus-visible:ring-transparent">
+            <svg class="h-5 w-5" fill="currentColor" viewBox="0 0 20 20" aria-hidden="true">
+              <path fill-rule="evenodd" d="M10 0C4.477 0 0 4.484 0 10.017c0 4.425 2.865 8.18 6.839 9.504.5.092.682-.217.682-.483 0-.237-.008-.868-.013-1.703-2.782.605-3.369-1.343-3.369-1.343-.454-1.158-1.11-1.466-1.11-1.466-.908-.62.069-.608.069-.608 1.003.07 1.531 1.032 1.531 1.032.892 1.53 2.341 1.088 2.91.832.092-.647.35-1.088.636-1.338-2.22-.253-4.555-1.113-4.555-4.951 0-1.093.39-1.988 1.029-2.688-.103-.253-.446-1.272.098-2.65 0 0 .84-.27 2.75 1.026A9.564 9.564 0 0110 4.844c.85.004 1.705.115 2.504.337 1.909-1.296 2.747-1.027 2.747-1.027.546 1.379.203 2.398.1 2.651.64.7 1.028 1.595 1.028 2.688 0 3.848-2.339 4.695-4.566 4.942.359.31.678.921.678 1.856 0 1.338-.012 2.419-.012 2.747 0 .268.18.58.688.482A10.019 10.019 0 0020 10.017C20 4.484 15.522 0 10 0z" clip-rule="evenodd" />
+            </svg>
+            <span class="text-sm font-semibold leading-6">GitHub</span>
+          </a>
+        </div>
+      </div>
+    </div>
+
+    <p class="mt-10 text-center text-sm text-gray-500">
+      Not a member? An account will be created for you when you sign in.
+    </p>
+  </div>
+</div>
